bdbd878d81170fb0ca9eeb1390e5c1e21d8ae891
William <springfoxstudios@gmail.com>
2018-06-07 16:41:37 -0400

    Getters and Setters
    	public Vector3f(float x, float y, float z)
    	{
    		this.setX(x);
    		this.setY(y);
    		this.setZ(z);
    	}
    
    	public float getX() {
    		return x;
    	}
    
    	public void setX(float x) {
    		this.x = x;
    	}
    
    	public float getY() {
    		return y;
    	}
    
    	public void setY(float y) {
    		this.y = y;
    	}
    
    	public float getZ() {
    		return z;
    	}
    
    	public void setZ(float z) {
    		this.z = z;
    	}
    }

--- HSource Engine/HSource/src/springfox/hsource/main/Vector3f.java
+++ HSource Engine/HSource/src/springfox/hsource/main/Vector3f.java
@@ -8,8 +8,32 @@ public class Vector3f
 	
 	public Vector3f(float x, float y, float z)
 	{
+		this.setX(x);
+		this.setY(y);
+		this.setZ(z);
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
 		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
 		this.y = y;
+	}
+
+	public float getZ() {
+		return z;
+	}
+
+	public void setZ(float z) {
 		this.z = z;
 	}
 }
