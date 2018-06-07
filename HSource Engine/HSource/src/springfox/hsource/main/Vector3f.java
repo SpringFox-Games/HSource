8c25694a59f197019dcfd1699413ed039e8be492
William <springfoxstudios@gmail.com>
2018-06-07 16:40:36 -0400

    made new Vector
    package springfox.hsource.main;
    
    public class Vector3f
    {
    	private float x;
    	private float y;
    	private float z;
    
    	public Vector3f(float x, float y, float z)
    	{
    		this.x = x;
    		this.y = y;
    		this.z = z;
    	}
    }

---
+++ HSource Engine/HSource/src/springfox/hsource/main/Vector3f.java
@@ -0,0 +1,15 @@
+package springfox.hsource.main;
+
+public class Vector3f 
+{
+	private float x;
+	private float y;
+	private float z;
+	
+	public Vector3f(float x, float y, float z)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
