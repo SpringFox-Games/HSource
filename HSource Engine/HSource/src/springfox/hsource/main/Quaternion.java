b8a7e6fdcd35745df0fcfc6bc0afe718ef9402e7
William <springfoxstudios@gmail.com>
2018-06-07 17:36:32 -0400

    Quaternion Start
    +package springfox.hsource.main;
    
    public class Quaternion
    {
    	private float x;
    	private float y;
    	private float z;
    	private float w;
    
    	public Quaternion(float x, float y, float z, float w)
    	{
    		this.x = x;
    		this.y = y;
    		this.z = z;
    		this.w = w;
    	}
    }

---
+++ HSource Engine/HSource/src/springfox/hsource/main/Quaternion.java
@@ -0,0 +1,17 @@
+package springfox.hsource.main;
+
+public class Quaternion 
+{
+	private float x;
+	private float y;
+	private float z;
+	private float w;
+	
+	public Quaternion(float x, float y, float z, float w)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.w = w;
+	}
+}
