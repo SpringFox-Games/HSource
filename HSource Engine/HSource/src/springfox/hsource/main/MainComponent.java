31032fe4732bdc9ddd90b364feba38554cea79a4
William <springfoxstudios@gmail.com>
2018-06-07 14:32:18 -0400

    Render Methods and Window

--- HSource Engine/HSource/src/springfox/hsource/main/MainComponent.java
+++ HSource Engine/HSource/src/springfox/hsource/main/MainComponent.java
@@ -2,9 +2,53 @@ package springfox.hsource.main;
 
 public class MainComponent 
 {
+	
+	public static final int WIDTH = 1920;
+	public static final int HEIGHT = 1080;
+	public static final String TITLE = "HSource SDK";
+	
+	public MainComponent()
+	{
+		
+	}
+	
+	public void start()
+	{
+		run();
+	}
+	
+	public void stop()
+	{
+		
+	}
+	
+	public void run()
+	{
+		while(!Window.isCloseRequested())
+		{
+			render();
+		}
+	}
+	
+	public void render()
+	{
+		Window.render();
+	}
+	
+	public void cleanUp()
+	{
+		
+	}
+	
     public static void main(String[] args)
     {
     	System.out.println("@-- HSource");
     	System.out.println("@-- Version 0.0.1");
+    	System.out.println("Starting!");	
+    	Window.createWindow(WIDTH, HEIGHT, TITLE);
+    	
+    	MainComponent game = new MainComponent();
+    	
+    	game.start();
     }
 }
