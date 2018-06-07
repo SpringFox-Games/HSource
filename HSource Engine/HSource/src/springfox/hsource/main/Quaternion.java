9801d375df0ed5df96d93e31bc3a66217e15895e
William <springfoxstudios@gmail.com>
2018-06-07 17:38:15 -0400

    Getters and Setter
    	public Quaternion(float x, float y, float z, float w)
    	{
    		this.x = x;
    		this.setX(x);
    		this.setY(y);
    		this.setZ(z);
    		this.setW(w);
    	}
    
    	public float getY() {
    		return y;
    	}
    
    	public void setY(float y) {
    		this.y = y;
    	}
    
    	public float getX() {
    		return x;
    	}
    
    	public void setX(float x) {
    		this.x = x;
    	}
    
    	public float getZ() {
    		return z;
    	}
    
    	public void setZ(float z) {
    		this.z = z;
    	}
    
    	public float getW() {
    		return w;
    	}
    
    	public void setW(float w) {
    		this.w = w;
    	}
    }

--- HSource Engine/HSource/src/springfox/hsource/main/Quaternion.java
+++ HSource Engine/HSource/src/springfox/hsource/main/Quaternion.java
@@ -9,9 +9,41 @@ public class Quaternion
 	
 	public Quaternion(float x, float y, float z, float w)
 	{
-		this.x = x;
+		this.setX(x);
+		this.setY(y);
+		this.setZ(z);
+		this.setW(w);
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
 		this.y = y;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getZ() {
+		return z;
+	}
+
+	public void setZ(float z) {
 		this.z = z;
+	}
+
+	public float getW() {
+		return w;
+	}
+
+	public void setW(float w) {
 		this.w = w;
 	}
 }
