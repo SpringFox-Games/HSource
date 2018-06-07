1b1561eaca9bbd3ba6d777eb0406cc7faf6f6768
William <springfoxstudios@gmail.com>
2018-06-07 13:00:36 -0400

    libs

---
+++ HSource Engine/HSource/src/springfox/hsource/main/MainComponent.java
@@ -0,0 +1,10 @@
+package springfox.hsource.main;
+
+public class MainComponent 
+{
+    public static void main(String[] args)
+    {
+    	System.out.println("@-- HSource");
+    	System.out.println("@-- Version 0.0.1");
+    }
+}
---
+++ HSource Engine/HSource/src/springfox/hsource/main/Window.java
@@ -0,0 +1,9 @@
+package springfox.hsource.main;
+
+public class Window 
+{
+	public static void createWindow(int width, int height, String title)
+	{
+		
+	}
+}
