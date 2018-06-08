cb4e3aa0a98a737da8f775c627ce925ad5d4da84
William <springfoxstudios@gmail.com>
2018-06-07 20:46:56 -0400

    Open GL Stuff
    package springfox.hsource.main;
    
    import static org.lwjgl.opengl.GL11.*;
    
    public class RenderUtil
    {
    	public static void
    	public static void clearScreen()
    	{
    		//TODO: Stencil Buffer
    		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
    	}
    }

--- HSource Engine/HSource/src/springfox/hsource/main/RenderUtil.java
+++ HSource Engine/HSource/src/springfox/hsource/main/RenderUtil.java
@@ -1,6 +1,12 @@
 package springfox.hsource.main;
 
+import static org.lwjgl.opengl.GL11.*;
+
 public class RenderUtil 
 {
-	public static void
+	public static void clearScreen()
+	{
+		//TODO: Stencil Buffer
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	}
 }
