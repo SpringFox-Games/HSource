acf2aae736d6030e4d3b4462a7f17535b8c0c864
William <springfoxstudios@gmail.com>
2018-06-09 11:01:35 -0400

    Some Stuffs
    public class Transform
    {
    	private Vector3f translation;
    
    	public Transform()
    	{
    		translation = new Vector3f(0,0,0);
    	}
    
    	public Vector3f getTranslation() {
    		return translation;
    	}
    
    	public void setTranslation(Vector3f translation) {
    		this.translation = translation;
    	}
    
    	public void setTranslation(float x, float y, float z) {
    		this.translation = new Vector3f(x, y, z);
    	}
    }

--- HSource Engine/HSource/src/springfox/hsource/main/Transform.java
+++ HSource Engine/HSource/src/springfox/hsource/main/Transform.java
@@ -2,5 +2,22 @@ package springfox.hsource.main;
 
 public class Transform 
 {
+	private Vector3f translation;
+			
+	public Transform()
+	{
+		translation = new Vector3f(0,0,0);
+	}
 
+	public Vector3f getTranslation() {
+		return translation;
+	}
+
+	public void setTranslation(Vector3f translation) {
+		this.translation = translation;
+	}
+	
+	public void setTranslation(float x, float y, float z) {
+		this.translation = new Vector3f(x, y, z);
+	}
 }
